af15ac2232765be389f3b153cf5bd381f7ffcde0
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-08-16 15:55:19 +0900

    docs: [user] auth middleware 생성 및 board 폴더 생성 user와 관계 명시

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { UserModule } from './user/user.module';
 import { typeOrmModuleAsyncOptions } from './configs/database.config';
@@ -6,8 +6,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { configModuleValidationSchema } from './configs/env-validation.config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { AuthInterceptor } from '../src/auth/auth.interceptor';
+// import { APP_INTERCEPTOR } from '@nestjs/core';
+// import { AuthInterceptor } from '../src/auth/auth.interceptor';
+import { AuthMiddleware } from './auth/auth.middleware';
+import { BoardModule } from './board/board.module';
 
 @Module({
   imports: [
@@ -18,8 +20,13 @@ import { AuthInterceptor } from '../src/auth/auth.interceptor';
     TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
     AuthModule,
     UserModule,
+    BoardModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('reviews', 'services'); // 미들웨어를 적용할 라우트
+  }
+}
