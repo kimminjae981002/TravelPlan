5de481f5159caaea198abf698db78d68d3ba79ee
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-09-25 23:16:00 +0900

    fix: [frontend] multer 수정 중

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -40,12 +40,13 @@ export class BoardController {
    */
   @Post()
   @UseInterceptors(
-    FileInterceptor('file', {
+    FileInterceptor('image', {
       storage: diskStorage({
         destination: (req, file, cb) => {
           cb(null, join(__dirname, '..', '..', 'uploads')); // uploads 폴더 경로
         },
         filename: (req, file, cb) => {
+          console.log(file);
           cb(null, file.originalname); // 파일 이름을 원래 이름으로 설정
         },
       }),
