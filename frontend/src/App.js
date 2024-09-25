ca84eff715fb848ef02c587632e04c3cde77e0b1
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-09-25 13:43:42 +0900

    feat: [frontend] react bootstrap before

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,99 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Header(props) {
+  return (
+    <hedaer>
+      <h1>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </hedaer>
+  );
+}
+
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={'/read/' + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            // event.target은 클릭했을 때 발생하는 것 - a tag
+            props.onChangeMode(event.target.id);
+          }}
+        >
+          {t.title}
+        </a>
+      </li>,
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
 
 function App() {
+  const [mode, setMode] = useState('Welcome');
+  const [id, setId] = useState(null);
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' },
+  ];
+  let content = null;
+  if (mode === 'Welcome') {
+    content = <Article title="Welcome" body="Welcome to my MJBlog"></Article>;
+  } else if (mode === 'READ') {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (+topics[i].id === +id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header
+        title="MJBlog"
+        onChangeMode={() => {
+          setMode('Welcome');
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode('READ');
+          setId(_id);
+        }}
+      ></Nav>
+      {content}
     </div>
   );
 }
