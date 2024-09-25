c19fc38f4daf7d80c5a8d35cd790689c8fe08720
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-09-25 16:00:56 +0900

    chore: [main] user dto 수정

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -20,11 +20,11 @@ export class CreateUserDto {
   @ApiProperty({
     description: '비밀번호',
     required: true,
-    example: 'abc123456@',
+    example: 'abc123456',
   })
   @IsStrongPassword(
-    { minLength: 8, minNumbers: 1, minSymbols: 1, minUppercase: 0 },
-    { message: '비밀번호는 특수문자/알파벳/숫자를 포함해야 합니다.' },
+    { minLength: 8 },
+    { message: '비밀번호는 8자리 이상입니다.' },
   )
   @IsNotEmpty({ message: '비밀번호를 작성해주세요.' })
   @MaxLength(20)
@@ -33,7 +33,7 @@ export class CreateUserDto {
   @ApiProperty({
     description: '비밀번호 체크',
     required: true,
-    example: 'abc123456@',
+    example: 'abc123456',
   })
   @IsStrongPassword(
     { minLength: 8, minNumbers: 1, minSymbols: 1, minUppercase: 0 },
