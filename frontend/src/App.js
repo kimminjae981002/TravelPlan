18ed4fc0b363e6f726dab438b48ed287ad814383
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-09-25 14:03:56 +0900

    feat: [frontend] react-bootstrap navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,99 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
-function Header(props) {
-  return (
-    <hedaer>
-      <h1>
-        <a
-          href="/"
-          onClick={(event) => {
-            event.preventDefault();
-            props.onChangeMode();
-          }}
-        >
-          {props.title}
-        </a>
-      </h1>
-    </hedaer>
-  );
-}
-
-function Nav(props) {
-  const lis = [];
-  for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(
-      <li key={t.id}>
-        <a
-          id={t.id}
-          href={'/read/' + t.id}
-          onClick={(event) => {
-            event.preventDefault();
-            // event.target은 클릭했을 때 발생하는 것 - a tag
-            props.onChangeMode(event.target.id);
-          }}
-        >
-          {t.title}
-        </a>
-      </li>,
-    );
-  }
-  return (
-    <nav>
-      <ol>{lis}</ol>
-    </nav>
-  );
-}
-
-function Article(props) {
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      {props.body}
-    </article>
-  );
-}
+import HomePage from './pages/HomePage';
+import Navigation from './components/navbar/Navigation';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [mode, setMode] = useState('Welcome');
-  const [id, setId] = useState(null);
-  const topics = [
-    { id: 1, title: 'html', body: 'html is ...' },
-    { id: 2, title: 'css', body: 'css is ...' },
-    { id: 3, title: 'javascript', body: 'javascript is ...' },
-  ];
-  let content = null;
-  if (mode === 'Welcome') {
-    content = <Article title="Welcome" body="Welcome to my MJBlog"></Article>;
-  } else if (mode === 'READ') {
-    let title,
-      body = null;
-    for (let i = 0; i < topics.length; i++) {
-      if (+topics[i].id === +id) {
-        title = topics[i].title;
-        body = topics[i].body;
-      }
-    }
-    content = <Article title={title} body={body}></Article>;
-  }
   return (
-    <div>
-      <Header
-        title="MJBlog"
-        onChangeMode={() => {
-          setMode('Welcome');
-        }}
-      ></Header>
-      <Nav
-        topics={topics}
-        onChangeMode={(_id) => {
-          setMode('READ');
-          setId(_id);
-        }}
-      ></Nav>
-      {content}
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/navbar/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Navigation = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">MJBlog</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
