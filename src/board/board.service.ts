10ce34b703aa6d8e45ac964d75f188d56c2f36aa
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-09-28 22:50:24 +0900

    fix: [main] Netlify 설정

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -20,10 +20,10 @@ export class BoardService {
     private readonly boardRepository: Repository<Board>,
   ) {
     this.s3Client = new S3Client({
-      region: process.env.AWS_REGION,
+      region: process.env.MY_AWS_REGION,
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
       },
     });
   }
