fd736d4e61bda272d266d55fca340e2dd7816225
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-10-03 22:07:26 +0900

    feat: [main] swagger 수정

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -5,7 +5,7 @@ export class CreateUserDto {
   @ApiProperty({
     description: '유저 ID',
     required: true,
-    example: 'testid',
+    example: 'test',
   })
   @IsNotEmpty({ message: 'ID를 작성해주세요.' })
   @MinLength(4, { message: '아이디는 4글자 이상 20글자 이하로 해주세요.' })
@@ -15,7 +15,7 @@ export class CreateUserDto {
   @ApiProperty({
     description: '비밀번호',
     required: true,
-    example: 'abc123456',
+    example: '123123123',
   })
   @Length(8, 20, { message: '비밀번호는 최소 8자리 이상이어야 합니다.' })
   @IsNotEmpty({ message: '비밀번호를 작성해주세요.' })
@@ -24,7 +24,7 @@ export class CreateUserDto {
   @ApiProperty({
     description: '비밀번호 체크',
     required: true,
-    example: 'abc123456',
+    example: '123123123',
   })
   @Length(8, 20, { message: '비밀번호는 최소 8자리 이상이어야 합니다.' })
   @IsNotEmpty({ message: '비밀번호를 작성해주세요.' })
